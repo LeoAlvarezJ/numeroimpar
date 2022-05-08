2c7f348406d323191b6eb3ebd280885b2ef886a6
CintiaAlvarez <104107300+CintiaAlvarez@users.noreply.github.com>
2022-05-07 22:48:14 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let numero: number = 0;
+while (numero <= 0) {
+numero = Number(prompt("Ingrese un numero"));
+
+}
+if (numero % 2 === 0) {
+console.log("El numero ingresado es par");
+}  else {
+console.log("El numero ingresado es impar");
+}
+});
